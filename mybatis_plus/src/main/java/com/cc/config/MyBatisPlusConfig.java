15c9809d4e319e8ab4bd8273c4e88c70f76d56be
chengcheng1021 <chengcheng1021@aliyun.com>
2020-09-23 15:41:40 +0800

    性能分析插件

--- mybatis_plus/src/main/java/com/cc/config/MyBatisPlusConfig.java
+++ mybatis_plus/src/main/java/com/cc/config/MyBatisPlusConfig.java
@@ -7,6 +7,7 @@ import com.baomidou.mybatisplus.extension.plugins.OptimisticLockerInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.aop.interceptor.PerformanceMonitorInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
